3637d00afb3ba009b6af3e14857f26e1019b0dfc
Alessandro Clerici <ale.clericil@gmail.com>
2021-06-26 15:11:33 +0200

    added sbrk

---
+++ mars-syscalls/sbrk.asm
@@ -0,0 +1,42 @@
+.macro push reg
+	addi $sp, $sp, -4
+	sw \reg, 0($sp)
+.endm
+
+.macro pop reg
+	lw \reg, 0($sp)
+	addi $sp, $sp, 4
+.endm
+
+	.text
+	.globl sbrk
+# sbrk(a0) allocates a0 bytes after the program break.
+#
+# Parameters:
+# a0: number of bytes to allocate
+#
+# Return value:
+# address of the allocated memory if success, -1 otherwise
+sbrk:
+	push $s0
+	move $s0, $a0
+
+	li $v0, 4045
+	li $a0, 0
+	syscall
+	move $s1, $v0
+
+	li $v0, 4045
+	add $a0, $s1, $s0
+	syscall
+
+	beq $v0, $s1, error
+	move $v0, $s1
+
+end:
+	pop $s0
+	jr $ra
+
+error:
+	li $v0, -1
+	j end
