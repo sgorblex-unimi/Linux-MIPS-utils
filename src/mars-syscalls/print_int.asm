86a6d6be657c863f721a06d03b812bd547e055e3
Alessandro Clerici <ale.clericil@gmail.com>
2021-07-10 21:15:02 +0200

    better print_int (stack based)

--- src/mars-syscalls/print_int.asm
+++ src/mars-syscalls/print_int.asm
@@ -1,4 +1,4 @@
-.include "stack.asm"
+.include "syscalls.asm"
 
 	.text
 
@@ -7,80 +7,39 @@
 #
 # Parameters:
 # a0: number to be printed
-#
-# Errors:
-# a3 = 1 if failed brk or write
 print_int:
-	push $s0
-	push $s1
-	push $s2
-	push $s3
-
-	move $s0, $a0
-
-	li $v0, 4045
-	li $a0, 0
-	syscall			# brk(0)
-	move $s1, $v0
-
-	# allocate 11 bytes
-	li $v0, 4045
-	addi $a0, $s1, 11
-	syscall 		# brk(pr_bk + 11)
-
-	# check brk success
-	beq $v0, $s1, error
-	move $a1, $v0		# v0: br_bk = first address out of string
-	move $s2, $v0
-
-
-	slt $s3, $s0, $zero
-	beqz $s0, zero
-	li $t0, 10
-	beqz $s3, loop
-	addi $s0, $s0, -1
-	not $s0, $s0
-
-loop:
-	beqz $s0, checkneg	# end loop if s0=0
-	addi $a1, $a1, -1
-	div $s0, $t0
-	mflo $s0		# s0 /= 10
-	mfhi $t1		# t1 = s0%10
-	addi $t1, $t1, '0'
-	sb $t1, 0($a1)
-	j loop
-
-zero:
-	li $t1, '0'
-	addi $a1, $a1, -1
-	sb $t1, 0($a1)
-
-checkneg:
-	# minus if negative
-	beqz $s3, print
-	li $t1, '-'
-	addi $a1, $a1, -1
-	sb $t1, 0($a1)
-
-print:
-	sub $a2, $s2, $a1
-	li $a0, 1
-	li $v0, 4004
-	syscall
-
-	li $v0, 4045
-	move $a0, $s1
-	syscall
-	beq $v0, $s2, error
+	move $t1, $sp		# t1: temporary stack pointer
 	
-end:
-	pop $s3
-	pop $s2
-	pop $s1
-	pop $s0
-	jr $ra
+	beqz $a0, zero
 
-error:
-	li $a3, 1
-	j end
+	move $t7, $a0		# copy of a0 to later determine sign
+	addi $t2, $zero, 10
+	bgtz $a0, loop
+	
+	addi $a0, $a0, -1
+	not $a0, $a0		# two's complement
+loop:
+	addi $t1, $t1, -1
+	div $a0, $t2
+	mflo $a0		# a0 /= 10
+	mfhi $t0		# t0 = old a0 % 10
+	addi $t0, $t0, '0'
+	sb $t0, 0($t1)
+	beqz $a0, store_sign
+	j loop
+store_sign:
+	bgtz $t7, print
+	addi $t1, $t1, -1
+	addi $t0, $zero, '-'
+	sb $t0, 0($t1)
+print:
+	addi $a0, $zero,  1
+	move $a1, $t1
+	sub $a2, $sp, $t1
+	write
+	jr $ra
+zero:
+	addi $t1, $t1, -1
+	addi $t0, $zero, '0'
+	sb $t0, 0($t1)
+	j print
